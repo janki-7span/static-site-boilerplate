81fa7611e49686eb8f080d698536b28ea94d5335
janki-7span <janki@7span.com>
2022-11-29 05:33:27 +0530

    dist added

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,9 +1,11 @@
 module.exports = {
   "extends": "stylelint-config-standard",
   "plugins": ["stylelint-scss"],
+  "defaultSeverity": "warning",
+  "ignoreFiles": ["*/.js"],
   "rules": {
     "at-rule-no-unknown": null,
     "scss/at-rule-no-unknown": true,
-    "no-descending-specificity": null
+    "no-descending-specificity": null 
   },
 }
